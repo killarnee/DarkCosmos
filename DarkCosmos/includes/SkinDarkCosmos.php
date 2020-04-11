4ce27002704a4cff52c2736a1fb3ab7b6c745141
Killarnee <57904791+Killarnee@users.noreply.github.com>
2020-04-12 01:49:54 +0200

    Add files via upload

---
+++ DarkCosmos/includes/SkinDarkCosmos.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * SkinTemplate class for the DarkCosmos skin
+ *
+ * @ingroup Skins
+ */
+class SkinDarkCosmos extends SkinTemplate {
+    /** @var string */
+    public $skinname = 'darkcosmos';
+
+    /** @var string */
+    public $stylename = 'DarkCosmos';
+
+    /** @var string */
+    public $template = 'DarkCosmosTemplate';
+
+    /**
+     * @param OutputPage $out
+     */
+    public function initPage( OutputPage $out ) {
+        parent::initPage( $out );
+
+        $out->addMeta( 'viewport',
+            'width=device-width, initial-scale=1.0, ' .
+            'user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0'
+        );
+
+        $out->addModuleStyles( [
+            'mediawiki.skinning.content.externallinks',
+            'skins.darkcosmos',
+        ] );
+        $out->addModules( [
+            'skins.darkcosmos.js',
+            'skins.darkcosmos.mobile'
+        ] );
+
+        // Basic IE support without flexbox
+        $out->addStyle( $this->stylename . '/resources/IE9fixes.css', 'screen', 'IE' );
+    }
+
+    /**
+     * Add CSS via ResourceLoader
+     *
+     * @param OutputPage $out
+     */
+    public function setupSkinUserCss( OutputPage $out ) {
+        parent::setupSkinUserCss( $out );
+    }
+}
